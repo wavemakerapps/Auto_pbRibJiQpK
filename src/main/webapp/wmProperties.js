655122e8ca78f945bd44b1f24243b2faa65734f4
root <root@61949b3d2f95>
2017-07-05 01:06:20 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_pbRibJiQpK",
+  "homePage" : "Main",
+  "name" : "Auto_pbRibJiQpK",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
